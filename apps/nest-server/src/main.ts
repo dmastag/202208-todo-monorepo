00df2d54411b7fedab9c2d146ba848eff20785bd
Julian Alimin <julian.alimin@yahoo.com>
2022-08-15 23:08:49 +0700

    enable cors

--- apps/nest-server/src/main.ts
+++ apps/nest-server/src/main.ts
@@ -13,6 +13,18 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
 
