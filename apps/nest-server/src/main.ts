e639037f0e7433c0811a772d7c329fb66f3de1f8
Julian Alimin <julian.alimin@yahoo.com>
2022-08-15 22:11:44 +0700

    use swagger to show the API structure

--- apps/nest-server/src/main.ts
+++ apps/nest-server/src/main.ts
@@ -8,10 +8,23 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo example')
+    .setDescription('The todos API description')
+    .setVersion('1.0')
+    .addTag('todo')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('openapi', app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
